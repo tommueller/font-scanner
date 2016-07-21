4f632ec443fbac4ea4709e44e01afbd2932bc77b
Tom Müller <t.mueller@interfacema.de>
2016-07-21 13:13:24 +0200

    Update font-scanner to be able to run several pages in one run add sitemap-scanner.js to perform checks all pages of a sitemap

--- README.md
+++ README.md
@@ -1,29 +1,32 @@
 # font-scanner
 
-Phantomjs-Project to scan all used font-variations or combinations on a website. I created a companies website and did not include google fonts like you should be (add specific weight, style by time), but I added all of them and wanted to remove the unused one later on.
+This [phantomjs](http://phantomjs.org/)-project helps you to determine the amount of different fonts and font-styles (weight and italic) used across a page or a whole site.
 
-It will output a list of JSON-Objects following this form:
+It will output a list of used font-combinations in this form (delimiter is ';'):
 
-```json
-{
-    "font-family": "Roboto, Helvetica, Arial, sans-serif",
-    "font-style": "normal",
-    "font-weight": "400"
-}
-{
-    "font-family": "'Roboto Condensed', Helvetica, Arial, sans-serif",
-    "font-style": "normal",
-    "font-weight": "400"
-}
+```
+Roboto, Helvetica, Arial, sans-serif;400;normal
+Roboto Condensed, Helvetica, Arial, sans-serif;400;normal
 ```
 
 It uses the whole font-stack, the font-weight and font-style. Font-size can also be outputted (uncomment one line). This will probably need to be done several times, to have it for all screen sizes.
 
 ## Usage
+### To run a single page (_of course without the hyphens!_)
+
 ```
-phantomjs font-scanner.js 'some-url.com'
+phantomjs font-scanner.js 'http://some-url.com'
 ```
-_of course without the hyphens!_
+
+### To run multiple pages (_of course without the hyphens!_)
+```
+phantomjs font-scanner.js ''http://some-url.com' ''http://second-url.org' ... ''http://nth-url.net'
+```
+
+### To run all pages from a sitemap (_of course without the hyphens!_)
+```
+phantomjs font-scanner.js $(phantomjs sitemap-scanner.js 'http://yourdomain.com/sitemap.xml')
+```
 
 All output will be written to a file called 'style'.
 
@@ -33,3 +36,5 @@ Thanks to the phantomjs and jQuery developers. And also to the authors of these 
 http://stackoverflow.com/a/12823730/1423259
 
 http://stackoverflow.com/a/23508177/1423259
+
+http://stackoverflow.com/a/17000930/1423259
--- font-scanner.js
+++ font-scanner.js
@@ -1,67 +1,90 @@
-var page = require('webpage').create(),
-system = require('system'), t, address;
+var page = require('webpage').create(), system = require('system'), fs = require('fs');
+var path = 'styles', count = 1, allItems = [];
 
 if (system.args.length === 1) {
-  console.log('Usage: loadspeed.js <some URL>');
+  console.log('Usage: font-scanner.js <list of n URLs>');
   phantom.exit();
 }
 
-t = Date.now();
-address = system.args[1];
-page.open(address, function(status) {
-  if (status !== 'success') {
-    console.log('FAIL to load the address');
-    phantom.exit();
-  } else {
-
-    t = Date.now() - t;
-    console.log('Loading ' + system.args[1]);
-    console.log('Loading time ' + t + ' msec');
-
-    page.viewportSize = { width: 1920, height: 1200 };
-
-    var items = page.evaluate(function() {
-
-      function unique(obj){
-        var uniques=[];
-        var stringify={};
-        for(var i=0;i<obj.length;i++){
-          var keys=Object.keys(obj[i]);
-          keys.sort(function(a,b) {return a-b});
-          var str='';
-          for(var j=0;j<keys.length;j++){
-            str+= JSON.stringify(keys[j]);
-            str+= JSON.stringify(obj[i][keys[j]]);
-          }
-          if(!stringify.hasOwnProperty(str)){
-            uniques.push(obj[i]);
-            stringify[str]=true;
-          }
-        }
-        return uniques;
-      }
-
-      var styles = [];
-      var items = document.body.getElementsByTagName("*");
-
-      for (var i = 0; i < items.length; i++) {
-        var $this = items[i];
-        styles.push({
-          // "font-size": $($this).css('font-size'),
-          "font-family": $($this).css('font-family'),
-          "font-weight": $($this).css('font-weight'),
-          "font-style": $($this).css('font-style')
-        });
-      }
-
-      return unique(styles);
-    });
-
-    for (var i = 0; i < items.length; i++) {
-      var $this = items[i];
-      console.log(JSON.stringify($this, null, 4));
+// sort array and delete duplicates based on there properties
+function unique(obj){
+  var uniques=[];
+  var stringify={};
+  for(var i=0;i<obj.length;i++){
+    var keys=Object.keys(obj[i]);
+    keys.sort(function(a,b) {return a-b});
+    var str='';
+    for(var j=0;j<keys.length;j++){
+      str+= JSON.stringify(keys[j]);
+      str+= JSON.stringify(obj[i][keys[j]]);
     }
+    if(!stringify.hasOwnProperty(str)){
+      uniques.push(obj[i]);
+      stringify[str]=true;
+    }
+  }
+  return uniques;
+}
 
+// set viewportsize
+page.viewportSize = { width: 1920, height: 1200 };
+
+function handle_page(url){
+
+  console.log('Evaluating ' + url);
+
+  page.open(url, function(status){
+    if (status !== 'success') {
+      console.log('FAIL to load the address');
+      phantom.exit();
+    } else {
+
+      page.includeJs('https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js', function() {
+
+        var items = page.evaluate(function() {
+
+          var styles = [];
+          var items = document.body.getElementsByTagName("*");
+
+          for (var i = 0; i < items.length; i++) {
+            var $this = items[i];
+            styles.push({
+              // "font-size": $($this).css('font-size'),
+              "font-family": $($this).css('font-family').replace('\'', '').replace('\"', '').replace('\'', ''),
+              "font-weight": $($this).css('font-weight').replace('\'', '').replace('\"', '').replace('\'', ''),
+              "font-style": $($this).css('font-style').replace('\'', '').replace('\"', '').replace('\'', '')
+            });
+          }
+
+          styles.push({'url' : 'egal'});
+
+          return styles;
+        });
+
+        allItems = allItems.concat(items);
+        setTimeout(next_page(),100);
+      });
+    }
+  });
+}
+
+function next_page() {
+  var url = system.args[count++];
+  if(!url) {
+
+    var finalItems = unique(allItems);
+
+    for (var i = 0; i < finalItems.length; i++) {
+      var $this = finalItems[i];
+      if (i === 0) {
+        fs.write(path, $this['font-family'] + ';' + $this['font-weight'] + ';' + $this['font-style'] + '\n', 'w');
+      } else {
+        fs.write(path, $this['font-family'] + ';' + $this['font-weight'] + ';' + $this['font-style'] + '\n', 'a');
+      }
+    }
     phantom.exit();
   }
-});
+  handle_page(url);
+}
+
+next_page();
